624401e7b57588928b675bee90f349094f2d5424
Luminight <Luminight@luminight.dev>
2024-07-16 01:29:03 +0200

    add .nvimrc.lua

---
+++ .nvimrc.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<F5>", "<cmd>!bear -- make<CR>")
+vim.keymap.set("n", "<F6>", "<cmd>!./bin/main<CR>")
+vim.keymap.set("n", "<F7>", "<cmd>!gdb ./bin/main<CR>")
+vim.keymap.set("n", "<F8>", "<cmd>!make clean<CR>")
